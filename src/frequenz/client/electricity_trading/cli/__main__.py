98c37799542be0430eafd422d2687d434995792c
cwasicki <126617870+cwasicki@users.noreply.github.com>
2025-01-14 00:04:09 +0100

    Add CLI commands to list trades and orders via API
    Introduces CLI commands to interact with the trading API:
    - `list_trades` allows listing and streaming public trades for specified
      delivery periods. If no delivery start is given, starts streaming all
      new public trades.
    - `list_orders` allows listing and streaming orders for specified
      delivery periods and gridpool IDs. If no delivery start is given,
      starts streaming all new orders for this gridpool ID.
    
    Signed-off-by: cwasicki <126617870+cwasicki@users.noreply.github.com>

--- src/frequenz/client/electricity_trading/cli/__main__.py
+++ src/frequenz/client/electricity_trading/cli/__main__.py
@@ -3,12 +3,19 @@
 
 """CLI tool to interact with the trading API."""
 
+import asyncio
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
 import click
 
 from frequenz.client.electricity_trading.cli.day_ahead import list_day_ahead_prices
+from frequenz.client.electricity_trading.cli.etrading import (
+    list_orders as run_list_orders,
+)
+from frequenz.client.electricity_trading.cli.etrading import (
+    list_trades as run_list_trades,
+)
 
 TZ = ZoneInfo("Europe/Berlin")
 
@@ -26,6 +33,25 @@ def midnight(days: int = 0) -> str:
 @click.group()
 def cli() -> None:
     """CLI tool to interact with the trading API."""
+
+
+@cli.command()
+@click.option("--url", required=True, type=str)
+@click.option("--key", required=True, type=str)
+@click.option("--start", default=None, type=iso)
+def list_trades(url: str, key: str, *, start: datetime) -> None:
+    """List trades."""
+    asyncio.run(run_list_trades(url=url, key=key, delivery_start=start))
+
+
+@cli.command()
+@click.option("--url", required=True, type=str)
+@click.option("--key", required=True, type=str)
+@click.option("--start", default=None, type=iso)
+@click.option("--gid", required=True, type=int)
+def list_orders(url: str, key: str, *, start: datetime, gid: int) -> None:
+    """List orders."""
+    asyncio.run(run_list_orders(url=url, key=key, delivery_start=start, gid=gid))
 
 
 @cli.command()
